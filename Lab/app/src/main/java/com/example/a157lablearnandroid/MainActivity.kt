f4e128ff86124a53948ccd20685e6b605192595b
KrittinB <krittinboonsu48@gmail.com>
2026-02-03 16:34:53 +0700

    ....

--- Lab/app/src/main/java/com/example/a157lablearnandroid/MainActivity.kt
+++ Lab/app/src/main/java/com/example/a157lablearnandroid/MainActivity.kt
@@ -1,124 +1,121 @@
 package com.example.a157lablearnandroid
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Gray)
-                    .systemBarsPadding()
-                    .padding(top = 16.dp)
-            ) {
-                // hp
-                Box(
-                    modifier = Modifier
-                        .height(32.dp)
-                        .fillMaxWidth()
-                        .background(Color.White),
-                    contentAlignment = Alignment.CenterStart
-                ) {
-                    Text(
-                        text = "hp",
-                        modifier = Modifier
-                            .fillMaxWidth(0.57f)
-                            .background(Color.Red)
-                            .padding(horizontal = 8.dp),
-                        color = Color.White,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
-
-                Spacer(modifier = Modifier.height(24.dp))
 
 
-                // image
-                Box(
-                    modifier = Modifier.fillMaxWidth(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.puppy),
-                        contentDescription = "Profile Image",
-                        modifier = Modifier
-                            .size(150.dp)
-                            .clip(CircleShape)
-                            .background(Color.LightGray),
-                        contentScale = ContentScale.Crop
+            startActivity(
+                Intent(this@MainActivity, ListActivity::class.java)
+            )
 
-                    )
-                }
+            CharacterScreen()
 
-                Spacer(modifier = Modifier.height(24.dp))
 
-                // status label
-                Text(
-                    text = "Status",
-                    color = Color.Black,
-                    modifier = Modifier
-                        .padding(horizontal = 18.dp, vertical = 10.dp),
-                    fontWeight = FontWeight.Bold
-                )
-
-                // Status Row
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp),
-                    horizontalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    StatusItem(label = "Str", value = 99)
-                    StatusItem(label = "Agi", value = 85)
-                    StatusItem(label = "Int", value = 80)
-                }
             }
-        }
     }
 }
+@Preview
 
 @Composable
-fun StatusItem(label: String, value: Int) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(
-            text = label, 
-            color = Color.Black, 
-            fontWeight = FontWeight.Bold
+fun CharacterScreen() {
+
+
+    var str by remember { mutableStateOf(8) }
+    var agi by remember { mutableStateOf(10) }
+    var intStat by remember { mutableStateOf(15) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray)
+            .padding(32.dp)
+    ) {
+
+        // HP BAR
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(40.dp)
+                .background(Color.White)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.2f)
+                    .fillMaxHeight()
+                    .background(Color.Red)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.puppy),
+            contentDescription = "Profile",
+            modifier = Modifier
+                .size(20.dp)
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 16.dp)
+                .clickable {
+                    //startActivity(Intent(this@MainActivity, ListActivity::class.java))
+
+
+
+                }
         )
-        Text(
-            text = value.toString(), 
-            color = Color.White,
-            fontWeight = FontWeight.Bold
-        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            Column {
+                Button(onClick = { str++ }) {
+                    Text("+", fontSize = 32.sp)
+                }
+                Text("Str", fontSize = 32.sp)
+                Text(str.toString(), fontSize = 32.sp)
+            }
+
+            Column {
+                Button(onClick = { agi++ }) {
+                    Text("+", fontSize = 32.sp)
+                }
+                Text("Agi", fontSize = 32.sp)
+                Text(agi.toString(), fontSize = 32.sp)
+            }
+
+            Column {
+                Button(onClick = { intStat++ }) {
+                    Text("+", fontSize = 32.sp)
+                }
+                Text("Int", fontSize = 32.sp)
+                Text(intStat.toString(), fontSize = 32.sp)
+            }
+        }
     }
 }
