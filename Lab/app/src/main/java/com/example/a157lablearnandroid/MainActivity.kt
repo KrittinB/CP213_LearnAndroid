00043a64f97c992b2cc7db5d1c4539bc5c4bf6f2
KrittinB <krittinboonsu48@gmail.com>
2026-02-04 00:03:39 +0700

    UpdaTe

--- Lab/app/src/main/java/com/example/a157lablearnandroid/MainActivity.kt
+++ Lab/app/src/main/java/com/example/a157lablearnandroid/MainActivity.kt
@@ -1,124 +1,132 @@
 package com.example.a157lablearnandroid
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Column(
+            RPGCardView()
+        }
+    }
+
+    @Composable
+    fun RPGCardView() {
+        val context = LocalContext.current
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.Gray)
+                .padding(32.dp)
+        ) {
+            // hp
+            Box(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Gray)
-                    .systemBarsPadding()
-                    .padding(top = 16.dp)
+                    .fillMaxWidth()
+                    .height(32.dp)
+                    .background(color = Color.White)
             ) {
-                // hp
-                Box(
-                    modifier = Modifier
-                        .height(32.dp)
-                        .fillMaxWidth()
-                        .background(Color.White),
-                    contentAlignment = Alignment.CenterStart
-                ) {
-                    Text(
-                        text = "hp",
-                        modifier = Modifier
-                            .fillMaxWidth(0.57f)
-                            .background(Color.Red)
-                            .padding(horizontal = 8.dp),
-                        color = Color.White,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
-
-                Spacer(modifier = Modifier.height(24.dp))
-
-
-                // image
-                Box(
-                    modifier = Modifier.fillMaxWidth(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.puppy),
-                        contentDescription = "Profile Image",
-                        modifier = Modifier
-                            .size(150.dp)
-                            .clip(CircleShape)
-                            .background(Color.LightGray),
-                        contentScale = ContentScale.Crop
-
-                    )
-                }
-
-                Spacer(modifier = Modifier.height(24.dp))
-
-                // status label
                 Text(
-                    text = "Status",
-                    color = Color.Black,
+                    text = "HP",
                     modifier = Modifier
-                        .padding(horizontal = 18.dp, vertical = 10.dp),
-                    fontWeight = FontWeight.Bold
+                        .align(alignment = Alignment.CenterStart)
+                        .fillMaxWidth(fraction = 0.55f)
+                        .background(color = Color.Red)
+                        .padding(8.dp)
                 )
+            }
 
-                // Status Row
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp),
-                    horizontalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    StatusItem(label = "Str", value = 99)
-                    StatusItem(label = "Agi", value = 85)
-                    StatusItem(label = "Int", value = 80)
+            // image
+            Image(
+                painter = painterResource(R.drawable.puppy),
+                contentDescription = "My Image",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 16.dp, bottom = 32.dp)
+                    .clickable {
+                        context.startActivity(Intent(context, PokedexActivity::class.java))
+                    }
+            )
+
+            var str by remember { mutableStateOf(8) }
+            var agi by remember { mutableStateOf(10) }
+            var int by remember { mutableStateOf(15) }
+
+            // status
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                Column {
+                    Button(onClick = { str += 1 }) {
+                        Text(text = "+", fontSize = 32.sp)
+                    }
+                    Text(text = "Str", fontSize = 32.sp)
+                    Text(text = str.toString(), fontSize = 32.sp)
+
+                    Button(onClick = { str -= 1 }) {
+                        Text(text = "-", fontSize = 32.sp)
+                    }
+                }
+                Column {
+                    Button(onClick = { agi += 1 }) {
+                        Text(text = "+", fontSize = 32.sp)
+                    }
+                    Text(text = "Agi", fontSize = 32.sp)
+                    Text(text = agi.toString(), fontSize = 32.sp)
+
+                    Button(onClick = { agi -= 1 }) {
+                        Text(text = "-", fontSize = 32.sp)
+                    }
+                }
+                Column {
+                    Button(onClick = { int += 1 }) {
+                        Text(text = "+", fontSize = 32.sp)
+                    }
+                    Text(text = "Int", fontSize = 32.sp)
+                    Text(text = int.toString(), fontSize = 32.sp)
+
+                    Button(onClick = { int -= 1 }) {
+                        Text(text = "-", fontSize = 32.sp)
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun StatusItem(label: String, value: Int) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(
-            text = label, 
-            color = Color.Black, 
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = value.toString(), 
-            color = Color.White,
-            fontWeight = FontWeight.Bold
-        )
+    @Preview
+    @Composable
+    fun PreviewScreen() {
+        RPGCardView()
     }
 }
